ef573db2802eedcdbc0983f5993c75cb07591607
umeshEasebuzz <105357029+umeshEasebuzz@users.noreply.github.com>
2022-09-28 16:57:35 +0530

    Add files via upload

---
+++ TestPodDemo/TestPodDemo/TestPodDemo.swift
@@ -0,0 +1,21 @@
+//
+//  TestPodDemo.swift
+//  TestPodDemo
+//
+//  Created by Umesh Pedde on 28/09/22.
+//
+
+import Foundation
+public final class TestPodDemo {
+
+    let name = "TestPodDemo"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
